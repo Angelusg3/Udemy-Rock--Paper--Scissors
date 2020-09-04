cbfc693bbab2189382b82fecf7de3af4ef508501
Naty T <nathan11002@gmail.com>
2020-09-04 19:19:05 +0300

    prompt added

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-    console.log('Game is Starting...');
-}
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK} , ${PAPER} or ${SCISSORS}`, '').toUpperCase();
+    if (
+      selection !== ROCK &&
+      selection !== PAPER &&
+      selection !== SCISSORS
+    ) {
+        alert(`Invalid Choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', function(){
+        console.log('Game is Starting...');
+        const playerSelection = getPlayerChoice();
+        console.log(playerSelection);
+});
